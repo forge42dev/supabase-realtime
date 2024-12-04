5df677cff13ec50508e38f0297b728fb33304406
Alem Tuzlak <t.zlak@hotmail.com>
2024-12-04 18:18:44 +0100

    Upgrade (#2)
    * Upgrade
    
    * Upgrade
    
    * unsubscribe from channels, register new players joining in
    
    * fix
    
    * fix
    
    * fix
    
    * fix

---
+++ app/realtime/room.ts
@@ -0,0 +1,20 @@
+import type { RealtimeMessage } from "@supabase/supabase-js"
+import { queryClient } from "~/root"
+import type { RoomLoaderData } from "~/routes/rooms.$roomId"
+
+export const handleRoomUpdate = (roomId: string) => (payload: RealtimeMessage["payload"]) => {
+	const newRoomInfo = payload.new
+	// biome-ignore lint/style/noNonNullAssertion: This will be there
+	const data = queryClient.getQueryData<RoomLoaderData>(["room", roomId])!
+	if (payload.new.id !== data.room.id) return
+	queryClient.setQueryData<typeof data>(["room", data.room.id], {
+		...data,
+		room: {
+			...data.room,
+			...newRoomInfo,
+			flippedIndices: newRoomInfo.flippedIndices ?? data.room.flippedIndices,
+			matchedPairs: newRoomInfo.matchedPairs ?? data.room.matchedPairs,
+			currentTurn: newRoomInfo.current_turn,
+		},
+	})
+}
--- app/routes/rooms.$roomId.tsx
+++ app/routes/rooms.$roomId.tsx
@@ -1,11 +1,15 @@
+import { useQuery } from "@tanstack/react-query"
 import { QRCodeSVG } from "qrcode.react"
-import { useEffect, useState } from "react"
-import { type LoaderFunctionArgs, redirect, useRevalidator, useSubmit } from "react-router"
+import { useEffect } from "react"
+import { type LoaderFunctionArgs, redirect, useSubmit } from "react-router"
 import { Grid } from "~/components/game/Grid"
 import { Leaderboard } from "~/components/game/Leaderboard"
 import { VictoryScreen } from "~/components/game/VictoryScreen"
 import { db } from "~/db.server"
 import { getUserFromRequest } from "~/queries/user.server"
+import { handlePlayerUpdate } from "~/realtime/player"
+import { handleRoomUpdate } from "~/realtime/room"
+import { queryClient } from "~/root"
 import { supabase } from "~/utils/supabase"
 import type { Route } from "./+types/rooms.$roomId"
 
@@ -51,6 +55,10 @@ export async function loader({ params, request }: LoaderFunctionArgs) {
 	return { room, players: room.activePlayers, cards: room.cards, user, qrCode }
 }
 
+export type RoomLoaderData = Awaited<ReturnType<typeof loader>>
+export type Player = Awaited<ReturnType<typeof loader>>["players"][0]
+export type Room = Awaited<ReturnType<typeof loader>>["room"]
+
 export const action = async ({ request }: Route.ActionArgs) => {
 	const formData = await request.formData()
 	const playerId = formData.get("playerId") as string | null
@@ -70,25 +78,60 @@ export const action = async ({ request }: Route.ActionArgs) => {
 	return redirect("/")
 }
 
-export type Player = Awaited<ReturnType<typeof loader>>["players"][0]
-export type Room = Awaited<ReturnType<typeof loader>>["room"]
+export const clientLoader = async ({ serverLoader, params }: Route.ClientLoaderArgs) => {
+	// Try to get the data from the cache
+	const cachedData = queryClient.getQueryData<RoomLoaderData>(["room", params.roomId])
+	// Either used the cached data or fetch it from the server
+	const data = cachedData ?? (await serverLoader())
+	// Don't set the data if it's already there
+	if (!cachedData) {
+		queryClient.setQueryData(["room", params.roomId], data)
+	}
+	// Subscribe to real-time updates on the room
+	const roomSubscription = supabase
+		.channel("rooms")
+		.on("postgres_changes", { event: "*", schema: "public", table: "rooms" }, handleRoomUpdate(params.roomId))
+		.subscribe()
+	// Subscribe to real-time updates on the active players
+	const playerSubscription = supabase
+		.channel("active_players")
+		.on(
+			"postgres_changes",
+			{ event: "*", schema: "public", table: "active_players" },
+			handlePlayerUpdate(params.roomId, serverLoader)
+		)
+		.subscribe()
 
-export default function Room({ loaderData }: Route.ComponentProps) {
-	const { room, players, cards, user, qrCode } = loaderData
-	const [activeRoom, setActiveRoom] = useState<Room>(room)
-	const [activePlayers, setActivePlayers] = useState<Player[]>(players)
-	const showVictory = activeRoom.status === "complete"
+	const unsubscribe = () => {
+		roomSubscription.unsubscribe()
+		playerSubscription.unsubscribe()
+	}
+	return {
+		...data,
+		unsubscribe,
+	}
+}
+
+clientLoader.hydrate = true
+
+export default function Room({ loaderData, params }: Route.ComponentProps) {
+	const { room, cards, user, qrCode } = loaderData
 	const submit = useSubmit()
+	const { data } = useQuery<Route.ComponentProps["loaderData"]>({ queryKey: ["room", params.roomId] })
+	// Close the channels when the user leaves the page
 	useEffect(() => {
-		setActiveRoom(room)
-	}, [room])
+		return () => {
+			if ("unsubscribe" in loaderData) {
+				loaderData.unsubscribe()
+			}
+		}
+	}, [loaderData])
+	if (!data) return <div>Loading...</div>
+	const activeRoom = data.room
+	const activePlayers = data.players
+	const showVictory = activeRoom.status === "complete"
 
-	useEffect(() => {
-		setActivePlayers(players)
-	}, [players])
-
-	const { revalidate } = useRevalidator()
-	const markAsInactive = () => {
+	const leaveGame = () => {
 		const formData = new FormData()
 		const activePlayer = activePlayers.find((p) => p.playerId === user.id)
 		if (!activePlayer) return
@@ -96,113 +139,51 @@ export default function Room({ loaderData }: Route.ComponentProps) {
 		formData.append("playerId", user.id)
 		submit(formData, { method: "POST" })
 	}
-	// Subscribe to real-time updates
-	useEffect(() => {
-		const roomSubscription = supabase
-			.channel("rooms")
-			.on("postgres_changes", { event: "*", schema: "public", table: "rooms" }, (payload) => {
-				const newItem = payload.new as {
-					id: string
-					created_at: Date
-					gridSize: number
-					status: string
-					created_by: string
-					current_turn: string | null
-					winnerId: string | null
-					matchedPairs: string[]
-					flippedIndices: number[]
-					cards: string[]
-					name: string
-				}
-				setActiveRoom({
-					...room,
-					...newItem,
-					flippedIndices: newItem.flippedIndices ?? room.flippedIndices,
-					matchedPairs: newItem.matchedPairs ?? room.matchedPairs,
-					currentTurn: newItem.current_turn,
-				})
-			})
-			.subscribe()
-
-		const playerSubscription = supabase
-			.channel("active_players")
-			.on("postgres_changes", { event: "*", schema: "public", table: "active_players" }, (payload) => {
-				const newItem = payload.new as {
-					player_id: string
-					isActive: boolean
-					score: number
-					id: string
-					scrollPosition: {
-						x: number
-						y: number
-					}
-				}
-
-				if (!activePlayers.find((p) => p.playerId === newItem.player_id)) {
-					revalidate()
-				}
-				setActivePlayers(
-					activePlayers.map((player) => {
-						if (player.playerId === newItem.player_id) {
-							return {
-								...player,
-								isActive: newItem.isActive,
-								score: newItem.score,
-								scrollPosition: newItem.scrollPosition,
-							}
-						}
-						return player
-					})
-				)
-
-				//revalidate()
-			})
-			.subscribe()
-
-		return () => {
-			roomSubscription.unsubscribe()
-			playerSubscription.unsubscribe()
-		}
-	}, [revalidate, activePlayers, room])
-
+	// Update room data
+	const updateRoom = async (data: Partial<Room & { current_turn?: string }>) => {
+		await supabase.from("rooms").update(data).eq("id", activeRoom.id)
+	}
+	const updatePlayer = async (data: Partial<Player>) => {
+		await supabase.from("active_players").update(data).eq("room_id", activeRoom.id).eq("player_id", user.id)
+	}
 	// Handle card clicks
 	const handleCardClick = async (index: number) => {
+		// Don't do anything if there are already two flipped cards
 		if (activeRoom.flippedIndices.length === 2) return
+		// Add the clicked card to the flipped indices
 		const newFlippedIndices = [...activeRoom.flippedIndices, index]
-		await supabase
-			.from("rooms")
-			.update({ flippedIndices: [...newFlippedIndices] })
-			.eq("id", room.id)
+		// Update the room with the new flipped indices so everyone can see the change
+		await updateRoom({ flippedIndices: [...newFlippedIndices] })
+		// Check if it's a match if two cards are flipped
 		if (activeRoom.flippedIndices.length === 1) {
+			// Check if the two cards are the same
 			const isMatch = cards[activeRoom.flippedIndices[0]] === cards[index]
 			const currentPlayerIndex = activePlayers.findIndex((p) => p.player?.name === activeRoom.currentTurn)
 			const currentPlayer = activePlayers[currentPlayerIndex]
+			// If it's a match, update the player's score and the room's status
 			if (isMatch) {
 				const newMatchedPairs = [...activeRoom.matchedPairs, cards[index]]
-				const isFinished = newMatchedPairs.length === room.gridSize * 2
-				await supabase
-					.from("active_players")
-					.update({ score: currentPlayer.score + 1 })
-					.eq("room_id", room.id)
-					.eq("player_id", user.id)
-				await supabase
-					.from("rooms")
-					.update({ matchedPairs: newMatchedPairs, status: isFinished ? "complete" : "waiting" })
-					.eq("id", room.id)
-				if (isFinished) {
-					return
-				}
+				// Check if the game is finished and update the room and the player
+				const isFinished = newMatchedPairs.length === activeRoom.gridSize * 2
+				// Update the player's score
+				await updatePlayer({ score: currentPlayer.score + 1 })
+				// Update the room's status and matched pairs
+				await updateRoom({ matchedPairs: newMatchedPairs, status: isFinished ? "complete" : "waiting" })
+				// Revert the flipped cards so the game can go on
+				setTimeout(async () => {
+					await updateRoom({ flippedIndices: [] })
+				}, 1000)
+				// Exit early so that we don't go into the setTimeout below
+				return
 			}
 
 			setTimeout(async () => {
-				await supabase.from("rooms").update({ flippedIndices: [] }).eq("id", room.id)
-
 				const nextPlayer = activePlayers[currentPlayerIndex + 1]
-				if (nextPlayer === undefined) {
-					await supabase.from("rooms").update({ current_turn: activePlayers[0].player?.name }).eq("id", room.id)
-				} else {
-					await supabase.from("rooms").update({ current_turn: nextPlayer.player?.name }).eq("id", room.id)
-				}
+				// Move to the next player
+				await updateRoom({
+					flippedIndices: [],
+					current_turn: !nextPlayer ? activePlayers[0].player?.name : nextPlayer.player?.name,
+				})
 			}, 1000)
 		}
 	}
@@ -215,7 +196,7 @@ export default function Room({ loaderData }: Route.ComponentProps) {
 		<div className="bg-gray-50 min-h-screen">
 			<h1 className="bg-indigo-600 text-white drop-shadow-xl flex items-center justify-between text-center text-2xl font-bold p-4 lg:px-12">
 				{activeRoom.name}
-				<button type="button" className="bg-red-500 text-base text-white px-4 py-2 rounded-md" onClick={markAsInactive}>
+				<button type="button" className="bg-red-500 text-base text-white px-4 py-2 rounded-md" onClick={leaveGame}>
 					Leave
 				</button>
 			</h1>
@@ -224,7 +205,7 @@ export default function Room({ loaderData }: Route.ComponentProps) {
 					<div className="flex lg:flex-row flex-col justify-between gap-4">
 						<Grid
 							// biome-ignore lint/suspicious/noExplicitAny: <explanation>
-							size={activeRoom.gridSize as any}
+							size={room.gridSize as any}
 							cards={cards}
 							flippedIndices={activeRoom.flippedIndices}
 							matchedPairs={activeRoom.matchedPairs}
